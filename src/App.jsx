da118d07bc74d0c9816fb21af2342c97488200ad
Brandora <kevin@192.168.5.25>
2024-07-17 17:09:41 +0300

    framer motion

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Brandora from './components/Barandora'
+
+function App() {
+
+  return (
+    <>
+      <Brandora></Brandora>
+    </>
+  )
+}
+
+export default App
